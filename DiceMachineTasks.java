13c12753712e1a9f85e39285ec19aaea526ecd1e
gabbiiiiiii <146883419+gabbiiiiiii@users.noreply.github.com>
2024-01-22 08:39:42 -1000

    Update and rename Pseudocode.java to DiceMachineTasks.java
    This is where I code for my Dice Machine Project.

--- DiceMachineTasks.java
+++ DiceMachineTasks.java
@@ -1,27 +1,4 @@
-import java.util.Scanner;
 
-public class Pseudocode {
-
-	public static void main(String[] args) {
-		
-		//create a scanner to scan for the first and second numbers
-		Scanner numberScanner = new Scanner(System.in);
-		
-		//Print statement asking user for an input 
-		System.out.println("Please enter two numbers seperated by 'd'.");
-		
-		//Instantiate the user's input as a string
-		String userInput = new String(numberScanner.nextLine());
-		
-		//create 'd' as a border between the first and second number
-		int border = userInput.indexOf('d');
-		
-		//Utilize a substring method to determine the first number
-		//by starting at 0, we set a inclusive parameter starting at the first character
-		//by setting our border as our second parameter, we include everything
-		//before the d as our "firstNum"
-		String firstNum = userInput.substring(0, border);
-		
 		//Utilize a substring method to determine the first number
 		//by setting the parameter as the border+1, we include everything
 		//after the border as the second number
